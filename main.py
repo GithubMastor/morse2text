70618c59d718e90b1b6c701b6a5770cabbcb73ef
GithubMastor <seoreilan90@gmail.com>
2022-10-14 15:31:35 -0500

    complete text to morse code translater

---
+++ main.py
@@ -0,0 +1,19 @@
+from colorama import Fore, Back, Style
+import pandas
+
+nato_dict = pandas.read_csv("morse.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in nato_dict.iterrows()}
+
+def text_to_morse():
+    word = input(Fore.RESET + 'Input Your Message: ').upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print(Fore.RED + "Please only input english Alpha-Numeric Characters")
+        text_to_morse()
+    else:
+        message = ' '.join(output_list)
+        print(message)
+if __name__ == '__main__':
+    text_to_morse()
+
